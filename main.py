5723d1027fd13f4314d1950547da91f99b1003b2
Raymond Overfield <Raymond.Overfield88@gmail.com>
2023-10-01 18:20:15 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from resources import dictionary
+
+print(dictionary["a"])
+
+continue_prog = True
+while continue_prog:
+
+    text_to_convert = input("Enter text to convert to morse code: ")
+    print(text_to_convert)
