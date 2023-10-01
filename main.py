b16b1fa2ccc1584b2fff723e4c8ba3cacb4edbc8
Raymond Overfield <Raymond.Overfield88@gmail.com>
2023-10-01 18:43:47 +0300

    Working and commented program

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from resources import dictionary
 
-print(dictionary["a"])
-
 continue_prog = True
 while continue_prog:
 
-    text_to_convert = input("Enter text to convert to morse code: ")
-    print(text_to_convert)
+    text_to_convert = input("Enter text to convert to morse code: ").lower()
+
+    if text_to_convert == "quit":
+        continue_prog = False
+        print("The program is closing now.")
+    else:
+        morse = ""
+        for character in text_to_convert:
+            #print(character)
+            if character in dictionary:
+                # The space between letters is three units, therefore 3 spaces
+                morse += dictionary[character] + "   "
+            elif character == " ":
+                # The space between words is seven units, therefore add 4 spaces when space
+                morse += "    "
+
+        print(morse)
